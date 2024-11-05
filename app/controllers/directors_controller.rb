fca16afee3722aa639747d54c98ee04f66e7ec28
jake77895 <jspitzer@chicagobooth.edu>
2024-11-05 04:16:53 +0000

    Destroy Directors Added

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,37 @@
 class DirectorsController < ApplicationController
+  
+  def destroy 
+
+    the_id = params.fetch("an_id")
+
+    matching_records = Director.where({:id => the_id})
+
+    the_director = matching_records.at(0)
+
+    the_director.destroy
+
+    redirect_to("/directors")
+
+  end
+  
+  def create
+    
+    # Parameters: {"the_name"=>"Fredp", "the_dob"=>"2024-10-10", "the_bio"=>"Test", "the_image"=>"Test"}
+
+    the_director = Director.new
+    the_director.name = params.fetch("the_name")
+    the_director.dob = params.fetch("the_dob")
+    the_director.bio = params.fetch("the_bio")
+    the_director.image = params.fetch("the_image")
+
+    the_director.save
+
+    redirect_to("/directors")
+
+  end
+  
+  
+  
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
